d9c263062f4f97aab848f919a73da05c1e90085c
mateuskb <mateuskb2@gmail.com>
2020-02-11 16:52:18 -0200

    Config to apache connection

--- web/back/public/index.php
+++ web/back/public/index.php
@@ -3,9 +3,12 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
+
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
+
+$app->setBasePath("/pibiti/web/back/public");
 
 $app->get('/', function (Request $request, Response $response, $args) {
     $response->getBody()->write("Hello world!");
--- web/front/inc/js/api/consts.js
+++ web/front/inc/js/api/consts.js
@@ -1,5 +1,5 @@
 module.exports = Object.freeze({
 
-    URL_PATH: 'http://localhost:8080'
+    URL_PATH: 'http://localhost/pibiti/web/back/public'
 
 });
