56ae9e4a5e02af20b9906338261be2537f167fde
mateuskb <mateuskb@icloud.com>
2020-02-10 23:00:55 -0200

    Front http request

---
+++ web/front/inc/js/api/Requests.js
@@ -0,0 +1,48 @@
+var constants = require('./consts');
+
+const axios = require('axios');
+
+// GET
+async function makeGetRequest() {
+  
+  let res = await axios.get(constants.URL_PATH + '/hello/teste');
+
+  let data = res.data;
+  console.log(data);
+}
+
+makeGetRequest();
+/*
+// GET w/ HEADERS
+async function makeRequest() {
+
+    const config = {
+        method: 'get',
+        url: 'http://localhost:8080/hello/teste',
+        headers: { 'User-Agent': 'Console app' }
+    }
+
+    let res = await axios(config)
+
+    console.log(res.request._header);
+}
+
+makeRequest();
+
+// POST
+async function makePostRequest() {
+
+    params = {
+        id: 6,
+        first_name: 'Fred',
+        last_name: 'Blair',
+        email: 'freddyb34@gmail.com'
+      }
+
+    let res = await axios.post('http://localhost:3000/users/', params);
+
+    console.log(res.data);
+}
+
+makePostRequest();
+*/
---
+++ web/front/inc/js/api/consts.js
@@ -0,0 +1,5 @@
+module.exports = Object.freeze({
+
+    URL_PATH: 'http://localhost:8080'
+
+});
