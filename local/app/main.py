55cafc2dcd7aae6c9354e0555e681baf88926373
mateuskb <mateuskb@icloud.com>
2020-03-05 14:44:27 -0300

    App v0.1

--- local/app/main.py
+++ local/app/main.py
@@ -1,119 +1,137 @@
 #!/var/www/html/pibiti/local/app/venv/bin/python3.7
- 
-import pygame
- 
 
-def AAfilledRoundedRect(surface,rect,color,radius=0.4):
+import pygame, math, sys, os
+from time import sleep
 
-    """
-    AAfilledRoundedRect(surface,rect,color,radius=0.4)
+pygame.init()
 
-    surface : destination
-    rect    : rectangle
-    color   : rgb or rgba
-    radius  : 0 <= radius <= 1
-    """
+#Variables
+darkYellow = 204,204,0
+grey = 102, 102, 102
+light_grey = 243, 243, 243
+ccc = 204,204,204
+black = 0, 0, 0
+white = 255, 255, 255
+red = 255,0,0
+usual_red = 203,30,53
+marroon = 120, 0, 0
+green = 0,255,0
+light_green = 35,168,78
+blue = 0,0,255
+darkBlue = 0,0,128
 
-    rect         = pygame.Rect(rect)
-    color        = pygame.Color(*color)
-    alpha        = color.a
-    color.a      = 0
-    pos          = rect.topleft
-    rect.topleft = 0,0
-    rectangle    = pygame.Surface(rect.size,pygame.SRCALPHA)
+resolution = 800, 800
+my_font = pygame.font.SysFont("Times New Roman", 30)
+my_font2 = pygame.font.SysFont("Times New Roman", 15)
+my_font3 = pygame.font.SysFont("Times New Roman", 60)
 
-    circle       = pygame.Surface([min(rect.size)*3]*2,pygame.SRCALPHA)
-    pygame.draw.ellipse(circle,(0,0,0),circle.get_rect(),0)
-    circle       = pygame.transform.smoothscale(circle,[int(min(rect.size)*radius)]*2)
+start_x = 275
+start_y = 200
+rectWidth = 250
+rectHeight = 80
 
-    radius              = rectangle.blit(circle,(0,0))
-    radius.bottomright  = rect.bottomright
-    rectangle.blit(circle,radius)
-    radius.topright     = rect.topright
-    rectangle.blit(circle,radius)
-    radius.bottomleft   = rect.bottomleft
-    rectangle.blit(circle,radius)
+def clock_page():
+    window.fill(ccc)
 
-    rectangle.fill((0,0,0),rect.inflate(-radius.w,0))
-    rectangle.fill((0,0,0),rect.inflate(0,-radius.h))
+    if running:
+        button = pygame.draw.rect(window, (usual_red), (start_x, start_y, rectWidth, rectHeight), 0)
+        log = pygame.draw.rect(window, (light_grey), (50, 400, 700, 350), 0)
+    else:
+        button = pygame.draw.rect(window, (light_green), (start_x, start_y, rectWidth, rectHeight), 0)
 
-    rectangle.fill(color,special_flags=pygame.BLEND_RGBA_MAX)
-    rectangle.fill((255,255,255,alpha),special_flags=pygame.BLEND_RGBA_MIN)
+        
+    for element in elements:
+        
+        if element[0] == 'end' and not running:            
+            pass
+        elif element[0] == 'start' and running:  
+            pass
+        else:
+            window.blit(element[1], element[2]) 
+            
 
-    return surface.blit(rectangle,pos)
 
-
-def main():
-    # Initialize the game engine
-    pygame.init()
-    
-    # Define some colors
-    BLACK = [0, 0, 0]
-    WHITE = [255, 255, 255]
-    BLUE = [0, 0, 255]
-    GREEN = [0, 255, 0]
-    RED = [255, 0, 0]
-    
-    PI = 3.141592653
-
-    running = False
-    
-    # Loop until the user clicks the close button.
-    clock = pygame.time.Clock()
-    
-    # Set the height and width of the screen
-    size = [800, 800] # Screen resolution
-    
-    screen = pygame.display.set_mode(size)
-    
-    #button = AAfilledRoundedRect(screen, (275, 250, 250, 150), color=GREEN, radius=0.3)  # creates a rounded rect object
-    
-    # Clear the screen and set the screen background
-    screen.fill(WHITE)
-
-    button = AAfilledRoundedRect(screen, (275, 250, 250, 150), color=GREEN, radius=0.3)  # creates a rounded rect object
-
-    # Select the font to use, size, bold, italics
-    font = pygame.font.SysFont('Calibri', 25, True, False)
-
-    # Update display
     pygame.display.update()
-
-    # Loop as long as True
-    while True:
-        for event in pygame.event.get():  # User did something
-            if event.type == pygame.QUIT:  # If user clicked close
-                return False
-            
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                mouse_pos = event.pos  # gets mouse position
-
-                # checks if mouse position is over the button
-
-                if button.collidepoint(mouse_pos):
-                    # prints current location of mouse
-                    print('button was pressed at {0}'.format(mouse_pos))
-                    if running:
-                        pygame.draw.rect(screen, GREEN, button)
-                        running = False
-                    else:
-                        pygame.draw.rect(screen, RED, button)
-                        running = True
-
-
-        pygame.draw.rect(screen, GREEN, button)
-
-            
-
-
-        # This limits the while loop to a max of 60 times per second.
-        # Leave this out and we will use all CPU we can.
-        pygame.display.update()
-        clock.tick(60)
     
-    # Be IDLE friendly
-    pygame.quit()
-    sys.exit
+def main():
+    global window, clock
+    clock = pygame.time.Clock()
+    window = pygame.display.set_mode(resolution)
+    window.fill(ccc)
+    pygame.display.set_caption('Módulo control')
+    main_menu()
+
+
+def main_menu():
+    global elements, running
+
+    # Local variables
+    running = False
+
+    # Draw buttons
+    button = pygame.draw.rect(window, (light_green), (start_x, start_y, rectWidth, rectHeight), 0)
+    start_button = my_font3.render("Iniciar", 2, ccc)
+    end_button = my_font3.render("Desligar", 2, ccc)
+
+    # Draw title
+    title = my_font3.render("Controle do módulo", 2, grey)
+
+    
+   # Insert into the window
+    elements = [
+        ['titulo', title, (170, 70)],
+        ['start', start_button, (start_x + 48, start_y + 6)],
+        ['end', end_button, (start_x + 28, start_y + 6)]
+    ]
+
+    cur_path = os.path.dirname(__file__)
+    log_file = open(cur_path+ "/log/log.txt", 'w')
+    logs_file = open(cur_path+ "/log/logs.txt", 'a')
+
+    while True:
+        for event in pygame.event.get():
+            
+            if event.type == pygame.QUIT:
+                return False
+
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    if button.collidepoint(event.pos):
+                        if running:
+                            running = False
+                        else:
+                            running = True
+        
+        clock_page()
+
+        if running:
+            text = game()
+            sleep(0.1)
+            log_file.write(text + '\n')
+            logs_file.write(text + '\n')
+            write_log()
+            
+        clock.tick(60)
+        
+    log_file.close()
+    logs_file.close()
+
+def game(): 
+    sleep(1)
+    return 'Running'
+    
+def write_log():
+    print("------------------------")
+    text = ''
+    cur_path = os.path.dirname(__file__)
+    with open(cur_path+ "/log/logs.txt", 'r') as log:
+        lines = log.read().splitlines()
+        for line in lines[-50: -1]:
+            text += line + '\n'
+    
+    print(text)
+
+
 
 if __name__ == "__main__":
     main()
