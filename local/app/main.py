15af252b9c6e80980aed7a515b52fa832058f2c2
mateuskb <mateuskb@icloud.com>
2020-03-06 12:40:20 -0300

    Line wrapping

--- local/app/main.py
+++ local/app/main.py
@@ -2,6 +2,7 @@
 
 import pygame, math, sys, os
 from time import sleep
+from itertools import chain
 
 pygame.init()
 
@@ -29,6 +30,48 @@ start_x = 275
 start_y = 200
 rectWidth = 250
 rectHeight = 80
+
+log_pos = [60, 410]
+
+
+def truncline(text, font, maxwidth):
+        real=len(text)       
+        stext=text           
+        l=font.size(text)[0]
+        cut=0
+        a=0                  
+        done=1
+        old = None
+        while l > maxwidth:
+            a=a+1
+            n=text.rsplit(None, a)[0]
+            if stext == n:
+                cut += 1
+                stext= n[:-cut]
+            else:
+                stext = n
+            l=font.size(stext)[0]
+            real=len(stext)               
+            done=0                        
+        return real, done, stext       
+
+
+def wrapline(text, font, maxwidth): 
+    done=0                      
+    wrapped=[]                  
+                            
+    while not done:             
+        nl, done, stext=truncline(text, font, maxwidth) 
+        wrapped.append(stext.strip())                  
+        text=text[nl:]                                 
+    return wrapped
+        
+def wrap_multi_line(text, font, maxwidth):
+    """ returns text taking new lines into account.
+    """
+    lines = chain(*(wrapline(line, font, maxwidth) for line in text))
+    return list(lines)
+
 
 def clock_page():
     window.fill(ccc)
@@ -84,10 +127,6 @@ def main_menu():
         ['end', end_button, (start_x + 28, start_y + 6)]
     ]
 
-    cur_path = os.path.dirname(__file__)
-    log_file = open(cur_path+ "/log/log.txt", 'w')
-    logs_file = open(cur_path+ "/log/logs.txt", 'a')
-
     while True:
         for event in pygame.event.get():
             
@@ -105,31 +144,40 @@ def main_menu():
         clock_page()
 
         if running:
-            text = game()
-            sleep(0.1)
-            log_file.write(text + '\n')
-            logs_file.write(text + '\n')
-            write_log()
+            game()
             
         clock.tick(60)
         
-    log_file.close()
-    logs_file.close()
-
 def game(): 
-    sleep(1)
-    return 'Running'
+    text = 'Running'
+
+    cur_path = os.path.dirname(__file__)
+
+    with open(cur_path+ "/log/logs.txt", 'a') as logs_file:
+        logs_file.write(text + '\n')
+
+    read_log()
+    sleep(0.1)
     
-def write_log():
+def read_log():
     print("------------------------")
     text = ''
     cur_path = os.path.dirname(__file__)
     with open(cur_path+ "/log/logs.txt", 'r') as log:
         lines = log.read().splitlines()
-        for line in lines[-50: -1]:
-            text += line + '\n'
     
-    print(text)
+    lines = lines[-10: -1]
+    lines = wrap_multi_line(lines, my_font2, 390)
+    
+    labels = []
+    for line in lines[-11: -1]:
+        l = my_font2.render(line, 2, grey)
+        labels.append(l)
+
+    for line in range(len(labels)):
+        window.blit(labels[line],(log_pos[0],log_pos[1]+(line*15)+(15*line)))
+
+    #print(help(labels[0]))
 
 
 
